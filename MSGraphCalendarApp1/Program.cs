40cc4980b10bc28b8f008dc279fc929b1766d75a
Thuan Pham <35915268+so-thuan-pham@users.noreply.github.com>
2020-03-11 13:34:10 +0700

    Add files via upload

---
+++ MSGraphCalendarApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSGraphCalendarApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            GraphHelper.CreateCalendar();
+            //GraphHelper.CreateEvents();
+            //await GraphHelper.GetEventsAsync();
+            //GraphHelper.GetUserDetailsAsync()
+            Console.ReadKey();
+        }
+    }
+}
